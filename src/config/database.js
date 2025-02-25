af056d14393eabff2bebbea7c4d2fa9db32d031d
Sooraj2003 <96384011+Sooraj2003@users.noreply.github.com>
2025-02-25 08:19:14 +0530

    updated db password

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://soorajnp:soorajbond2003@cluster0.qu5h6.mongodb.net/devTinder?retryWrites=true&w=majority&appName=Cluster0")
+    await mongoose.connect("mongodb+srv://soorajnp:grg72muT1zVmfGjO@cluster0.qu5h6.mongodb.net/devTinder?retryWrites=true&w=majority&appName=Cluster0")
 }
 
 
